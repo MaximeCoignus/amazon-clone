0b66554a02034df69c5b95944221f45a692f11be
maxime <maxime@fsbtech.com>
2020-09-17 10:50:58 +0200

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDdN92TnXHeY4JbndFPSDiN7-eKOQhhL1M",
+    authDomain: "challenge-884a9.firebaseapp.com",
+    databaseURL: "https://challenge-884a9.firebaseio.com",
+    projectId: "challenge-884a9",
+    storageBucket: "challenge-884a9.appspot.com",
+    messagingSenderId: "32046734543",
+    appId: "1:32046734543:web:6805ee64bf9e7029322bba"
+  };
